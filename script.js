61a31499026beef52f710dc67f186a245e8f452e
Isaac Chin <mrisaacchx@gmail.com>
2022-10-27 17:45:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let right_btn = document.getElementById('right_btn');
+let left_btn = document.getElementById('left_btn');
+let random_btn = document.getElementById('random_btn');
+
+right_btn.addEventListener('click', function() {
+    console.log('right button clicked');
+})
+
+
